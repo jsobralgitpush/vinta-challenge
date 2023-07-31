3dcbe4edbb333356e3708e2e38e46b2d6f54d7fd
ze <jose.sobral@poli.ufrj.br>
2023-07-31 10:48:17 -0300

    feat(github api integration): Add RepositoryService to github.py
    This class is responsible to call github API with repo url and return
    Repository Domain

--- common/api/github.py
+++ common/api/github.py
@@ -3,3 +3,11 @@ import requests
 class Repository:
     def __init__(self, data):
         self.data = data
+
+class RepositoryService:
+    @staticmethod
+    def fetch(username):
+        url = f'https://api.github.com/users/{username}/repos'
+        response = requests.get(url)
+        repositories = [Repository(repo_data) for repo_data in response.json()]
+        return repositories
