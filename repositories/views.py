23fb8bf97b9a7539303fbd8bb8cbce06ae19b109
ze <jose.sobral@poli.ufrj.br>
2023-07-31 17:51:27 -0300

    feat(github api integration): Add if / else to handle flow control of repo name
    Based on search of every github repositories array

--- repositories/views.py
+++ repositories/views.py
@@ -24,7 +24,10 @@ def repository_create_view(request):
     social_auth = user.social_auth.get(provider='github')
     access_token = social_auth.extra_data['access_token']
     repositories = RepositoryService.fetch_by_authenticated_user(access_token)
-    serializer = RepositorySerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+    if any(repo.data['name'] == repo_name  for repo in repositories):
+        serializer = RepositorySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response({'error': 'Repository does not exists.'}, status=status.HTTP_404_NOT_FOUND)
