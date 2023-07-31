0f8f5da54f8b19e4d30f9f9eebfb5a29fb45c4bd
ze <jose.sobral@poli.ufrj.br>
2023-07-31 17:49:14 -0300

    feat(github api integration): Get access_token based on social auth request

--- repositories/views.py
+++ repositories/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from githubmonitor.api.github import RepositoryService
 
 from .models import Commit
 from .serializers import CommitSerializer, RepositorySerializer
@@ -18,6 +19,10 @@ def commit_list_view(request):
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def repository_create_view(request):
+    user = request.user
+    social_auth = user.social_auth.get(provider='github')
+    access_token = social_auth.extra_data['access_token']
+    repositories = RepositoryService.fetch_by_authenticated_user(access_token)
     serializer = RepositorySerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
