7bdbd5c551552e48041691400aae0b8423123ea4
ze <jose.sobral@poli.ufrj.br>
2023-07-31 15:35:53 -0300

    feat(github api integration): Add fetch by repo name method

--- common/api/github.py
+++ common/api/github.py
@@ -11,3 +11,9 @@ class RepositoryService:
         response = requests.get(url)
         repositories = [Repository(repo_data) for repo_data in response.json()]
         return repositories
+    
+    @staticmethod
+    def fetch_by_repo_name(username, repo_name):
+        url = f'https://api.github.com/repos/{username}/{repo_name}'
+        response = requests.get(url)
+        return Repository(response.json())
