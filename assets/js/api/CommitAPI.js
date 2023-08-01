7f37f3ef5b8ab53f59f6d19884dcb5e54a79b31b
ze <jose.sobral@poli.ufrj.br>
2023-08-01 09:24:18 -0300

    feat(sidebar component): Add getRepos method to fetch API

--- assets/js/api/CommitAPI.js
+++ assets/js/api/CommitAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {reset} from 'redux-form';
 import store from '../store';
 import {
-  createRepositorySuccess, getCommitsSuccess,
+  createRepositorySuccess, getCommitsSuccess, getReposSuccess
 } from '../actions/CommitActions';
 
 export const getCommits = () => axios.get(`/api/commits/`)
@@ -10,11 +10,16 @@ export const getCommits = () => axios.get(`/api/commits/`)
     store.dispatch(getCommitsSuccess({...response.data}));
   });
 
-export const createRepository = (values, headers, formDispatch) => axios.post('/api/repositories/create', values, {headers})
+export const createRepository = (values, headers, formDispatch) => axios.post('/api/repositories/create/', values, {headers})
   .then((response) => {
     store.dispatch(createRepositorySuccess(response.data, true));
     formDispatch(reset('repoCreate'));
   }).catch((error) => {
     const err = error.response;
     console.log(err);
+  });
+
+export const getRepos = () => axios.get(`/api/repositories/list/`)
+  .then((response) => {
+    store.dispatch(getReposSuccess({...response.data}));
   });
