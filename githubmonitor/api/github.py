3ed21922402e2bb06f282e22b5d7bdb2ab4a0a99
ze <jose.sobral@poli.ufrj.br>
2023-08-01 15:16:54 -0300

    feat(github api integration): Add fetch repo commits function
    Plus, add data modeling object (Commit)

--- githubmonitor/api/github.py
+++ githubmonitor/api/github.py
@@ -1,5 +1,14 @@
 import requests
 
+class Commit:
+    def __init__(self, data):
+        self.sha = data['sha']
+        self.url = data['html_url']
+        self.author = data['commit']['author']['name'] if data['author'] else None
+        self.message = data['commit']['message']
+        self.date = data['commit']['author']['date']
+        self.avatar = data['author']['avatar_url'] if data['author'] else None
+
 class Repository:
     def __init__(self, data):
         self.data = data
@@ -28,3 +37,12 @@ class RepositoryService:
         response = requests.get(url, headers=headers)
         repositories = [Repository(repo_data) for repo_data in response.json()]
         return response.status_code, repositories
+    
+    @staticmethod
+    def fetch_repo_commits(access_token, username, repo_name):
+        url = f'https://api.github.com/repos/{username}/{repo_name}/commits'
+        headers = {'Authorization': f"token {access_token}"}
+        response = requests.get(url, headers=headers, params={})
+        commits = [Commit(commit_data) for commit_data in response.json()]
+        return response.status_code, commits
+    
