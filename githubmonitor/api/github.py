33a0016406e5f04cd97bd30451f9d91f4cfdad13
ze <jose.sobral@poli.ufrj.br>
2023-08-01 12:08:35 -0300

    feat(github api integration ): Add tuple as RepositoryService return

--- githubmonitor/api/github.py
+++ githubmonitor/api/github.py
@@ -11,18 +11,20 @@ class RepositoryService:
         headers = {'Authorization': f"token {access_token}"}
         response = requests.get(url, headers=headers)
         repositories = [Repository(repo_data) for repo_data in response.json()]
-        return repositories
-    
+        return response.status_code, repositories
+
     @staticmethod
     def fetch_by_repo_name(access_token, username, repo_name):
         url = f'https://api.github.com/repos/{username}/{repo_name}'
         headers = {'Authorization': f"token {access_token}"}
         response = requests.get(url, headers=headers)
-        return Repository(response.json())
+        repository = Repository(response.json())
+        return response.status_code, repository
 
     @staticmethod
     def fetch_by_authenticated_user(access_token):
         url = 'https://api.github.com/user/repos'
         headers = {'Authorization': f"token {access_token}"}
         response = requests.get(url, headers=headers)
-        return [Repository(repo_data) for repo_data in response.json()]
+        repositories = [Repository(repo_data) for repo_data in response.json()]
+        return response.status_code, repositories
