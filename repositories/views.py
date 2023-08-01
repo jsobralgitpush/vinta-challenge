977890dbd613f7b0656a7777b6753d13a7ef3773
José Sobral <63429525+jsobralgitpush@users.noreply.github.com>
2023-08-01 12:35:18 -0300

    Feat/GitHub api integration/validate repo backend (#2)
    * feat(github api integration): Add api/github.py file
    
    This file will be responsible for manage the github API integration
    dealing with all github entities
    
    * feat(github api integration): Add Repository Domain
    
    * feat(github api integration): Add RepositoryService to github.py
    
    This class is responsible to call github API with repo url and return
    Repository Domain
    
    * refactor(github api integration): Change name of fetch method
    
    Change to fetch_by_user to build another methods
    
    * feat(github api integration): Add fetch by repo name method
    
    * feat(github api integration): Add fetch by authenticated user method
    
    * refactor(github api integration): Add access_token argument and header
    
    Add it to every method to access github API
    
    * refactor(github api integration): Change dir of github.py to githubmonitor dir
    
    * feat(github api integration): Add package mark to test dir
    
    Using __init__.py file
    
    * test(github api integration): Add unit tests to github.py
    
    * feat(github api integration): Get access_token based on social auth request
    
    * feat(github api integration): Add repo name based on request data
    
    * feat(github api integration): Add if / else to handle flow control of repo name
    
    Based on search of every github repositories array
    
    * test(github api integration): Add test views.py first version
    
    Add test repository create view, test commit list view and add a setup
    to handle UserSocialAuth mock
    
    * feat(github api integration): Add status code on RepositoryService as tuple
    
    * feat(github api integration): Handle different status code on repository create
    
    Status code based on github API
    
    * test(github api integration): Fix test based on tuple return of RepoService
    
    * test(github api integration): Fix view tests to handle different status code
    
    * chore(github api integration): Remove last line from the file
    
    * test(github api integration): Remove github_test.py
    
    This file has not the test name convention of test_*.py
    
    * test(github api integration): Add test to cover Unknown error

--- repositories/views.py
+++ repositories/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from githubmonitor.api.github import RepositoryService
 
 from .models import Commit
 from .serializers import CommitSerializer, RepositorySerializer
@@ -18,7 +19,23 @@ def commit_list_view(request):
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def repository_create_view(request):
-    serializer = RepositorySerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+    repo_name = request.data.get('name')
+    user = request.user
+    social_auth = user.social_auth.get(provider='github')
+    access_token = social_auth.extra_data['access_token']
+    status_code, repositories = RepositoryService.fetch_by_authenticated_user(access_token)
+    
+    if status_code in [401, 403]:
+        return Response({'error': 'Unauthorized or forbidden.'}, status=status_code)
+    elif status_code == 422:
+        return Response({'error': 'Unprocessable Entity.'}, status=status_code)
+    elif status_code not in [200, 304]:
+        return Response({'error': 'Unknown error.'}, status=status_code)
+    
+    if any(repo.data['name'] == repo_name for repo in repositories):
+        serializer = RepositorySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    return Response({'error': 'Repository does not exist.'}, status=status.HTTP_404_NOT_FOUND)
