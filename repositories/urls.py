fc68110477ba1ef6438e507d4566682a159f2e28
ze <jose.sobral@poli.ufrj.br>
2023-08-01 08:13:49 -0300

    feat(sidebar component): Add api/repositories/list and /create
    Change the previous version of create to prevent Django route misleading

--- repositories/urls.py
+++ repositories/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import commit_list_view, repository_create_view
+from .views import commit_list_view, repository_create_view, repository_list_view
 
 app_name = 'repositories'
 
 urlpatterns = [
     path('api/commits/', commit_list_view, name='commits-list'),
-    path('api/repositories/', repository_create_view, name='repositories-create'),
+    path('api/repositories/create/', repository_create_view, name='repositories-create'),
+    path('api/repositories/list/', repository_list_view, name='repositories-list'),
 ]
