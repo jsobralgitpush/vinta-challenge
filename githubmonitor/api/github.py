f60d68eab36d4f244ee6fdfbabd4f1b7356f463d
ze <jose.sobral@poli.ufrj.br>
2023-08-03 16:31:50 -0300

    feat(github api integration): Add params argument to each endpoint

--- githubmonitor/api/github.py
+++ githubmonitor/api/github.py
@@ -14,22 +14,22 @@ class RepositoryService:
         }
     
     @staticmethod
-    def fetch_by_user(username):
+    def fetch_by_user(username, params=None):
         url = f'https://api.github.com/users/{username}/repos'
-        response = requests.get(url, headers=RepositoryService().__get_headers())
+        response = requests.get(url, headers=RepositoryService().__get_headers(), params=params)
         repositories = [Repository(repo_data) for repo_data in response.json()]
         return response.status_code, repositories
 
     @staticmethod
-    def fetch_by_repo_name(username, repo_name):
+    def fetch_by_repo_name(username, repo_name, params=None):
         url = f'https://api.github.com/repos/{username}/{repo_name}'
-        response = requests.get(url, headers=RepositoryService().__get_headers())
+        response = requests.get(url, headers=RepositoryService().__get_headers(), params=params)
         repository = Repository(response.json())
         return response.status_code, repository
 
     @staticmethod
-    def fetch_by_authenticated_user():
+    def fetch_by_authenticated_user(params=None):
         url = 'https://api.github.com/user/repos'
-        response = requests.get(url, headers=RepositoryService().__get_headers())
+        response = requests.get(url, headers=RepositoryService().__get_headers(), params=params)
         repositories = [Repository(repo_data) for repo_data in response.json()]
         return response.status_code, repositories
