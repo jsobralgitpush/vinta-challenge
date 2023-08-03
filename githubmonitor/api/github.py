0916bb09a4b044f198eb538f2503497db2132aa7
ze <jose.sobral@poli.ufrj.br>
2023-08-03 15:36:04 -0300

    refactor(security): Refactor RepositoryService
    To use GITHUB_ACCESS_TOKEN

--- githubmonitor/api/github.py
+++ githubmonitor/api/github.py
@@ -1,30 +1,35 @@
 import requests
+from decouple import config
 
 class Repository:
     def __init__(self, data):
         self.data = data
 
 class RepositoryService:
+
+    def __get_headers(self):
+        return {
+            'Authorization': f"token {config('GITHUB_ACCESS_TOKEN')}",
+            'Accept': 'application/vnd.github.v3+json'
+        }
+    
     @staticmethod
-    def fetch_by_user(access_token, username):
+    def fetch_by_user(username):
         url = f'https://api.github.com/users/{username}/repos'
-        headers = {'Authorization': f"token {access_token}"}
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=RepositoryService().__get_headers())
         repositories = [Repository(repo_data) for repo_data in response.json()]
         return response.status_code, repositories
 
     @staticmethod
-    def fetch_by_repo_name(access_token, username, repo_name):
+    def fetch_by_repo_name(username, repo_name):
         url = f'https://api.github.com/repos/{username}/{repo_name}'
-        headers = {'Authorization': f"token {access_token}"}
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=RepositoryService().__get_headers())
         repository = Repository(response.json())
         return response.status_code, repository
 
     @staticmethod
-    def fetch_by_authenticated_user(access_token):
+    def fetch_by_authenticated_user():
         url = 'https://api.github.com/user/repos'
-        headers = {'Authorization': f"token {access_token}"}
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=RepositoryService().__get_headers())
         repositories = [Repository(repo_data) for repo_data in response.json()]
         return response.status_code, repositories
