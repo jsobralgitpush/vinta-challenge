bb00b3f1e6f6a6cb80adbed7b844de0cf27f8d21
ze <jose.sobral@poli.ufrj.br>
2023-08-04 08:37:33 -0300

    feat(enhance frontend ux): Add author and repo name filters

--- assets/js/components/CommitList/index.js
+++ assets/js/components/CommitList/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+import { getCommits } from '../../api/CommitAPI';
 
 const CommitList = (props) => {
   const {commits} = props;
+
+  const handleAuthorClick = (author) => {
+    const url = `/api/commits/?author=${author}`;
+    getCommits(url);
+  }
+
+  const handleRepoClick = (repo) => {
+    const url = `/api/commits/?repository_name=${repo}`;
+    getCommits(url);
+  }
   return (
     <div>
       {commits.length !== 0 && (
@@ -18,18 +29,18 @@ const CommitList = (props) => {
                   <div className="avatar">
                     <img alt={commit.author} className="img-author" src={commit.avatar} />
                   </div>
-                  <div className="commit-details">
+                  <div className="commit-detailms">
                     <p>
                       {commit.message}
                     </p>
                     <small className="text-muted">
-                      {commit.author}
+                      <a href="javascript:void(0);" onClick={() => handleAuthorClick(commit.author)}>{commit.author}</a>
                       {' '}
                       authored
                       {' '}
                       on
                       {' '}
-                      {commit.repository}
+                      <a href="javascript:void(0);" onClick={() => handleRepoClick(commit.repository)}>{commit.repository}</a>
                       {' '}
                       at
                       {' '}
