008d2c3eecb3d9a7467343ccb1ed0801d86413ba
ze <jose.sobral@poli.ufrj.br>
2023-08-04 11:51:32 -0300

    test(sidebar component): Add a unit test to RepoList/index.js

---
+++ assets/js/__tests__/components/RepoList/index.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import RepoList from '../../../components/RepoList/index';
+import { getCommits } from '../../../api/CommitAPI';
+
+jest.mock('../../../api/CommitAPI');
+
+const mockData = [
+  {
+    id: 1,
+    name: 'repo-one',
+  },
+  {
+    id: 2,
+    name: 'repo-two',
+  },
+];
+
+describe('RepoList', () => {
+  it('renders correctly', () => {
+    render(<RepoList repos={mockData} />);
+
+    expect(screen.getByText('repo-one')).toBeInTheDocument();
+    expect(screen.getByText('repo-two')).toBeInTheDocument();
+  });
+
+  it('handles repository click correctly', () => {
+    render(<RepoList repos={mockData} />);
+  
+    fireEvent.click(screen.getByText('repo-one').parentElement);
+  
+    expect(getCommits).toHaveBeenCalledWith('/api/commits/?repository_name=repo-one');
+  });
+});
