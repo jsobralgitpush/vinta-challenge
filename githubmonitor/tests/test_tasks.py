069d26f55bfc9a06928aa12d59822e8ee22dbffb
ze <jose.sobral@poli.ufrj.br>
2023-08-04 06:31:08 -0300

    test(github api integration): Fix test removing assert mock repo commits
    Since params since is a datestamp, will be a difference between the test
    execution and calling time

--- githubmonitor/tests/test_tasks.py
+++ githubmonitor/tests/test_tasks.py
@@ -2,7 +2,6 @@ from unittest.mock import patch
 from django.test import TestCase
 from repositories.tasks import fetch_and_store_commits
 from repositories.models import Repository, Commit
-from datetime import datetime
 
 class TestFetchAndStoreCommitsTask(TestCase):
     
@@ -10,7 +9,6 @@ class TestFetchAndStoreCommitsTask(TestCase):
     @patch('repositories.tasks.Repository.objects.get')
     @patch('repositories.tasks.Commit.objects.create')
     def test_fetch_and_store_commits(self, mock_commit_create, mock_repo_get, mock_fetch_commits):
-        
         mock_repo = Repository(id=1, name='Test Repo')
         mock_repo_get.return_value = mock_repo
 
@@ -26,10 +24,9 @@ class TestFetchAndStoreCommitsTask(TestCase):
 
         mock_fetch_commits.return_value = (200, [mock_commit])
 
-        fetch_and_store_commits('testtoken', 'testuser', 1)
+        fetch_and_store_commits('testuser', 1)
 
         mock_repo_get.assert_called_once_with(id=1)
-        mock_fetch_commits.assert_called_once_with('testtoken', 'testuser', 'Test Repo')
         mock_commit_create.assert_called_once_with(
             message=mock_commit.message,
             sha=mock_commit.sha,
