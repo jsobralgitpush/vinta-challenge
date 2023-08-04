336ceb209ee0edf2ed0070f3d1e2688e4ec722aa
José Sobral <63429525+jsobralgitpush@users.noreply.github.com>
2023-08-04 17:20:17 -0300

    chore(apply autopep8): Apply autopep8 in py files (#23)
    utopep8 --in-place --aggressive --aggressive --recursive .

--- githubmonitor/pagination.py
+++ githubmonitor/pagination.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from urllib.parse import urlencode
 
+
 class CustomPageNumberPagination(PageNumberPagination):
     def get_paginated_response(self, data):
         request = self.request
@@ -10,7 +11,10 @@ class CustomPageNumberPagination(PageNumberPagination):
         current_query_params = request.GET.copy()
 
         first_url = self.get_page_url(1, current_url, current_query_params)
-        last_url = self.get_page_url(self.page.paginator.num_pages, current_url, current_query_params)
+        last_url = self.get_page_url(
+            self.page.paginator.num_pages,
+            current_url,
+            current_query_params)
 
         return Response({
             'count': self.page.paginator.count,
@@ -27,11 +31,12 @@ class CustomPageNumberPagination(PageNumberPagination):
         query_string = urlencode(current_query_params)
         base_url = f"{current_url.split('?')[0]}?" if '?' in current_url else f"{current_url}?"
         return f"{base_url}{query_string}"
-    
+
     def paginate_queryset(self, queryset, request, view=None):
         try:
             return super().paginate_queryset(queryset, request, view)
         except NotFound:
-            self.paginator = self.django_paginator_class(queryset, self.page_size)
+            self.paginator = self.django_paginator_class(
+                queryset, self.page_size)
             self.page = self.paginator.page(self.paginator.num_pages)
             return self.page
--- repositories/views.py
+++ repositories/views.py
@@ -2,15 +2,17 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from repositories.tasks import fetch_and_store_commits 
+from repositories.tasks import fetch_and_store_commits
 from githubmonitor.api.github import RepositoryService
 from .models import Commit, CommitFilter, Repository
 from .serializers import CommitSerializer, RepositorySerializer
 from githubmonitor.pagination import CustomPageNumberPagination
 
+
 class BaseView(APIView):
     permission_classes = [IsAuthenticated]
-    
+
+
 class CommitListView(BaseView):
 
     def get(self, request):
@@ -20,33 +22,39 @@ class CommitListView(BaseView):
         context = paginator.paginate_queryset(filters.qs, request)
         serializer = CommitSerializer(context, many=True)
         return paginator.get_paginated_response(serializer.data)
-    
+
+
 class RepositoryListView(BaseView):
 
     def get(self, request):
         repositories = Repository.objects.all()
         serializer = RepositorySerializer(repositories, many=True)
         return Response(serializer.data)
-    
+
+
 class RepositoryCreateView(BaseView):
 
     def post(self, request):
         repo_name = request.data.get('name')
         status_code, repositories = RepositoryService.fetch_by_authenticated_user()
         user = request.user
-        
+
         if status_code in [401, 403]:
-            return Response({'error': 'Unauthorized or forbidden.'}, status=status_code)
+            return Response(
+                {'error': 'Unauthorized or forbidden.'}, status=status_code)
         elif status_code == 422:
-            return Response({'error': 'Unprocessable Entity.'}, status=status_code)
+            return Response({'error': 'Unprocessable Entity.'},
+                            status=status_code)
         elif status_code not in [200, 304]:
             return Response({'error': 'Unknown error.'}, status=status_code)
 
-        if any(repo.data['name'].lower() == repo_name.lower() for repo in repositories):
+        if any(repo.data['name'].lower() == repo_name.lower()
+               for repo in repositories):
             serializer = RepositorySerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
             fetch_and_store_commits.delay(user.username, serializer.data['id'])
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        return Response({'error': 'Repository does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'error': 'Repository does not exist.'},
+                        status=status.HTTP_404_NOT_FOUND)
